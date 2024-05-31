a929307e406c1029de7bbf1187521a485f8b0969
Bryan Jones <bryanjones@Bryans-MBP.attlocal.net>
2024-05-31 09:46:49 -0500

    :lipstick: updated styling and created delete options

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -16,6 +16,10 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # Page for adding a new entry.
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
-    # # Page for editing an entry.
+    # Page for editing an entry.
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    # Page for deleting a topic
+    path('delete_topic/<int:topic_id>/', views.delete_topic, name='delete_topic'),
+    # Page for deleting an entry
+    path('delete_entry/<int:entry_id>/', views.delete_entry, name='delete_entry'),
 ]
